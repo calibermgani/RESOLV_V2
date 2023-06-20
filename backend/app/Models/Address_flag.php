5df10c81ba510f839e0bd0759170384f97289d2d
calibermgani <mgani@medcubics.com>
2023-06-20 15:59:59 +0530

    backend folder commit

---
+++ backend/app/Models/Address_flag.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Address_flag extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $connection = 'mysql2';
+    protected $fillable = [
+        'address_company', 'type', 'address_line_1', 'address_line_2', 'city', 'state', 'zip5', 'zip4', 'is_address_match', 'updated_at'
+    ];
+}

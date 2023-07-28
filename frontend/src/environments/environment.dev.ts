f63ae285cc13cc7df43cefe4154bc2114d1ec255
SelvaFront <selva@caliberfocus.com>
2023-07-28 15:10:32 +0530

    Changes for build

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from './environment.interface';
 export const environment:IEnvironment = {
     production: false,
     name:'dev',
-    apiUrl: 'http://35.226.72.203/avecarm/backend/public/index.php/api'
+    apiUrl: 'http://34.68.221.3/resolv_v2/backend/public/index.php/api'
   };

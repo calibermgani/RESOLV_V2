fd0159c31ca3027804dad3d04ac79331d540fc1a
SelvaFront <selva@caliberfocus.com>
2024-02-12 11:08:14 +0530

    Changes in Angular.json and environment

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 import { IEnvironment } from './environment.interface';
 export const environment:IEnvironment = {
     production: false,
-    name:'dev',
+    name:'development',
     apiUrl: 'http://35.222.109.4/resolv_v2_build/backend/public/index.php/api'
   };

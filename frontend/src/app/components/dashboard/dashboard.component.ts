c7681d3c40cc889ba9bac44b18ea39006c5a5f8b
selvaFront <selva@caliberfocus.com>
2024-03-18 15:55:43 +0530

    changes in graph

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -148,7 +148,7 @@ export class DashboardComponent implements OnInit {
       dataFormat = "json";
       dataSource1= {
         chart: {
-          caption: "Average Fastball Velocity",
+          caption: "ATB - Payer Classification",
           yaxisname: "Velocity (in mph)",
           subcaption: "[2005-2016]",
           numbersuffix: " mph",
@@ -209,7 +209,7 @@ export class DashboardComponent implements OnInit {
       };
       dataSource2 = {
         chart: {
-          caption: "Recommended Portfolio Split",
+          caption: "ATB - Touch Analysis",
           subcaption: "For a net-worth of $1M",
           showvalues: "1",
           showpercentintooltip: "0",
@@ -240,6 +240,212 @@ export class DashboardComponent implements OnInit {
           }
         ]
       };
+      dataSource3 = {
+        chart: {
+          caption: "ATB - Status Code",
+          subcaption: "Last month",
+          pyaxisname: "No. of Occurrence",
+          theme: "gammel",
+          showsecondarylimits: "0",
+          showdivlinesecondaryvalue: "0",
+          plottooltext:
+            "Due to $label, late arrivals count is : <b>$dataValue</b> of the total <b>$sum</b> employees",
+          drawcrossline: "1"
+        },
+        data: [
+          {
+            label: "Traffic",
+            value: "5680"
+          },
+          {
+            label: "Family Engagement",
+            value: "1036"
+          },
+          {
+            label: "Public Transport",
+            value: "950"
+          },
+          {
+            label: "Weather",
+            value: "500"
+          },
+          {
+            label: "Emergency",
+            value: "140"
+          },
+          {
+            label: "Others",
+            value: "68"
+          }
+        ]
+      }
+      dataSource4 = {
+        chart: {
+          caption: "MTD - Associates Performance",
+          subcaption: "Conversions as % of total",
+          xaxisname: "# Conversions",
+          yaxisname: "Cost Per Conversion",
+          numberprefix: "$",
+          theme: "gammel",
+          plottooltext: "$name : Share of total conversion: $zvalue%"
+        },
+        categories: [
+          {
+            verticallinealpha: "20",
+            category: [
+              {
+                label: "0",
+                x: "0"
+              },
+              {
+                label: "1500",
+                x: "1500",
+                showverticalline: "1"
+              },
+              {
+                label: "3000",
+                x: "3000",
+                showverticalline: "1"
+              },
+              {
+                label: "4500",
+                x: "4500",
+                showverticalline: "1"
+              },
+              {
+                label: "6000",
+                x: "6000",
+                showverticalline: "1"
+              }
+            ]
+          }
+        ],
+        dataset: [
+          {
+            data: [
+              {
+                x: "5540",
+                y: "16.09",
+                z: "30.63",
+                name: "Campaign 1"
+              },
+              {
+                x: "4406",
+                y: "12.74",
+                z: "24.36",
+                name: "Campaign 2"
+              },
+              {
+                x: "1079",
+                y: "15.79",
+                z: "5.97",
+                name: "Campaign 3"
+              },
+              {
+                x: "1700",
+                y: "8.27",
+                z: "9.4",
+                name: "Campaign 4"
+              },
+              {
+                x: "853",
+                y: "15.89",
+                z: "4.71",
+                name: "Campaign 5"
+              },
+              {
+                x: "1202",
+                y: "10.74",
+                z: "6.65",
+                name: "Campaign 6"
+              },
+              {
+                x: "2018",
+                y: "6.14",
+                z: "11.16",
+                name: "Campaign 7"
+              },
+              {
+                x: "413",
+                y: "19.83",
+                z: "2.28",
+                name: "Campaign 8"
+              },
+              {
+                x: "586",
+                y: "13.96",
+                z: "3.24",
+                name: "Campaign 9"
+              },
+              {
+                x: "184",
+                y: "15.82",
+                z: "1.02",
+                name: "Campaign 10"
+              },
+              {
+                x: "311",
+                y: "5.83",
+                z: "1.72",
+                name: "Campaign 11"
+              },
+              {
+                x: "35",
+                y: "10.76",
+                z: "0.19",
+                name: "Campaign 12"
+              },
+              {
+                x: "55",
+                y: "2.73",
+                z: "0.3",
+                name: "Campaign 13"
+              },
+              {
+                x: "6",
+                y: "21.22",
+                z: "0.03",
+                name: "Campaign 14"
+              }
+            ]
+          }
+        ]
+      };
+      dataSource5 = {
+        "chart": {
+          "caption": "Client Assistance Report",
+          "subCaption": "In MMbbl = One Million barrels",
+          "xAxisName": "Country",
+          "yAxisName": "Reserves (MMbbl)",
+          "numberSuffix": "K",
+          "theme": "gammel",
+        },
+        "data": [{
+          "label": "Venezuela",
+          "value": "290"
+        }, {
+          "label": "Saudi",
+          "value": "260"
+        }, {
+          "label": "Canada",
+          "value": "180"
+        }, {
+          "label": "Iran",
+          "value": "140"
+        }, {
+          "label": "Russia",
+          "value": "115"
+        }, {
+          "label": "UAE",
+          "value": "100"
+        }, {
+          "label": "US",
+          "value": "30"
+        }, {
+          "label": "China",
+          "value": "30"
+        }]
+      };
 
       // dataSource: Object;
       chartConfig: any;
@@ -262,7 +468,7 @@ export class DashboardComponent implements OnInit {
 
       this.dataSource = {
           "chart": {
-            "caption": "Countries With Most Oil Reserves [2017-18]",
+            "caption": "ATB - Aging Analysis",
             "subCaption": "In MMbbl = One Million barrels",
             "xAxisName": "Country",
             "yAxisName": "Reserves (MMbbl)",
